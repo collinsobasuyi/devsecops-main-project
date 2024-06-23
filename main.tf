9fdb83e3cc4df8dce6ac91767d2b348f297882d5
collinsobasuyi <collins2020job@gmail.com>
2024-06-23 20:48:27 +0100

    ned tf file created

--- main.tf
+++ main.tf
@@ -1,14 +1,28 @@
-provider "aws" {
-region = "eu-west-2" # London region
-}
-resource "aws_s3_bucket" "terraform-april-collins-obasuyi" {
-bucket = "terraform-april-collins-obasuyi"
-}
 terraform {
   backend "s3" {
-    # Replace this with your bucket name!
-    bucket         = "terraform-april-collins-obasuyi"
-    key            = "global/s3/terraform.tfstate"
-    region         = "eu-west-2"
+    bucket = "terraform-april-collins-obasuyi"
+    key    = "global/s3/terraform.tfstate"
+    region = "eu-west-2"
   }
+}
+
+provider "aws" {
+  region = "eu-west-2"
+}
+
+resource "aws_s3_bucket" "terraform_bucket" {
+  bucket = "terraform-april-collins-obasuyi"
+  tags = {
+    Name        = "terraform-april-collins-obasuyi"
+    Environment = "Dev"
+  }
+}
+
+resource "aws_s3_bucket_acl" "bucket_acl" {
+  bucket = aws_s3_bucket.terraform_bucket.bucket
+  acl    = "private"
+}
+
+output "bucket_name" {
+  value = aws_s3_bucket.terraform_bucket.bucket
 }
