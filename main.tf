77bab93e48c824cbc16dc7587b0a06326d5bf4f9
collinsobasuyi <collins2020job@gmail.com>
2024-06-23 21:02:47 +0100

    Update Terraform configuration and combined workflow with renamed bucket

--- main.tf
+++ main.tf
@@ -1,3 +1,7 @@
+provider "aws" {
+  region = "eu-west-2"
+}
+
 terraform {
   backend "s3" {
     bucket = "terraform-april-collins"
@@ -6,6 +10,15 @@ terraform {
   }
 }
 
-provider "aws" {
-  region = "eu-west-2"
+resource "aws_s3_bucket" "terraform_bucket" {
+  bucket = "terraform-april-collins"
+  tags = {
+    Name        = "terraform-april-collins"
+    Environment = "Dev"
+  }
+}
+
+resource "aws_s3_bucket_acl" "bucket_acl" {
+  bucket = aws_s3_bucket.terraform_bucket.bucket
+  acl    = "private"
 }
