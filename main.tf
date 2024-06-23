b0263e7b6ebc13b294ea7f6bf73eaffe3fed1441
collinsobasuyi <collins2020job@gmail.com>
2024-06-23 20:18:20 +0100

    test

---
+++ main.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+region = "eu-west-2" # London region
+}
+resource "aws_s3_bucket" "terraform-april-collins-obasuyi" {
+bucket = "terraform-april-collins-obasuyi"
+}
+terraform {
+  backend "s3" {
+    # Replace this with your bucket name!
+    bucket         = "terraform-april-collins-obasuyi"
+    region         = "eu-west-2"
+  }
+}
