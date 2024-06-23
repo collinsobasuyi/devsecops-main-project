114a31b16eb3d8e9c86a3dba0d579251062a07c6
collinsobasuyi <collins2020job@gmail.com>
2024-06-23 20:57:27 +0100

    Add Terraform configuration and combined workflow

--- main.tf
+++ main.tf
@@ -9,20 +9,3 @@ terraform {
 provider "aws" {
   region = "eu-west-2"
 }
-
-resource "aws_s3_bucket" "terraform_bucket" {
-  bucket = "terraform-april-collins-obasuyi"
-  tags = {
-    Name        = "terraform-april-collins-obasuyi"
-    Environment = "Dev"
-  }
-}
-
-resource "aws_s3_bucket_acl" "bucket_acl" {
-  bucket = aws_s3_bucket.terraform_bucket.bucket
-  acl    = "private"
-}
-
-output "bucket_name" {
-  value = aws_s3_bucket.terraform_bucket.bucket
-}
