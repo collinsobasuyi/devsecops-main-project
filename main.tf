652496014b3a1e48f64b0ea23aebae2275168acf
collinsobasuyi <collins2020job@gmail.com>
2024-06-23 21:22:23 +0100

    Add Terraform configuration and workflow to create S3 bucket

--- main.tf
+++ main.tf
@@ -14,11 +14,3 @@ resource "aws_s3_bucket_acl" "bucket_acl" {
   bucket = aws_s3_bucket.terraform_bucket.bucket
   acl    = "private"
 }
-
-terraform {
-  backend "s3" {
-    bucket = "terraform-april-collins"
-    key    = "global/s3/terraform.tfstate"
-    region = "eu-west-2"
-  }
-}
