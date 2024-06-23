ac9fc96de2b0f1c252bd779825cfb5934d79c738
collinsobasuyi <collins2020job@gmail.com>
2024-06-23 21:06:49 +0100

    Add Terraform configuration and combined workflow with renamed bucket

---
+++ backend.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+  region = "eu-west-2"
+}
+
+terraform {
+  backend "s3" {
+    bucket = "terraform-april-collins"
+    key    = "global/s3/terraform.tfstate"
+    region = "eu-west-2"
+  }
+}
